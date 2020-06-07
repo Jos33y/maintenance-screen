6b526678fee6f5ce65a71e81dc6fccfdddb036ea
Jos33y <josephlagbalu@gmail.com>
2020-06-07 23:43:30 +0100

    updated dbconnect

--- dbcon.php
+++ dbcon.php
@@ -1,7 +1,10 @@
 <?php
 
 session_start();
-$con = mysqli_connect("localhost", "root", "", "citizenparticipation");
+//$con = mysqli_connect("localhost", "root", "", "citizenparticipation");
+
+$con = mysqli_connect("127.0.0.1:49841", "azure", "6#vWHD_$", "citizenparticipation");
+
 
 
 ?>
