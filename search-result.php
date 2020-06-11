3d85fcf11ad2a5117853dea1f3152d1b7f837ce4
Jos33y <josephlagbalu@gmail.com>
2020-06-11 23:49:26 +0100

    previous button partially working

--- search-result.php
+++ search-result.php
@@ -86,32 +86,116 @@ include('header.php');
     //$version_link = ", Click " . "<a href='#' class='text-white'>" . "here" . "</a> " . " to bring up latest version";
 
     $version_link =" ";
+  
+?>
+
+<?php
+
+if(isset($_GET['govid'])){
+
+$edit_id = $_GET['govid'];
+
+$get_a = "select * from addresses where GovId = '$edit_id'";
+
+$run_edit = mysqli_query($con, $get_a);
+
+$row_edit = mysqli_fetch_array($run_edit);
+
+$aid = $row_edit['AddressId'];
+$gvid = $row_edit['GovId'];
+$cmpid = $row_edit['ComptrollerID'];
+$pbdnfm = $row_edit['PublicBodyNameFormal'];
+$gvtype = $row_edit['GovType'];
+$weburl = $row_edit['WebsiteURL'];
+$hqemail = $row_edit['HQemail'];
+$hqpaddress = $row_edit['HQphysicalAddress'];
+$hqpcity = $row_edit['HQphysicalCity'];
+$hqmzip = $row_edit['HQmailingZip'];
+$hqphone = $row_edit['HQphone'];
+$hqmaddress = $row_edit['HQmailingAddress'];
+$hqmcity = $row_edit['HQmailingCity'];
+$hqstate = $row_edit['HQState'];
+$foiaemail = $row_edit['FoiaEmail'];
+$foiaeaddress = $row_edit['FoiaMailingAddress'];
+$foiaphone = $row_edit['FoiaPhone'];
+$foiapaddress = $row_edit['FoiaPhysicalAddress'];
+$foiamcity = $row_edit['FoiaMailingCity'];
+$foiastate = $row_edit['FoiaState'];
+$foiamzip = $row_edit['FoiaMailingZip'];
+
+$timestamp = date("M-d-yy h:i:s A", strtotime( $row_edit['timestamp']));
+
+$get_govtype = "select * from govtype where govtypeid = '$gvtype'";
+
+$run_govtype = mysqli_query($con, $get_govtype);
+
+$row = mysqli_fetch_array($run_govtype);
+
+$bgrpid = $row['ballotgroupid'];
+
+$dept = $row['comptypecode'];
+
+$govfunction = $row['govfunction'];
+
+$investiture = $row['investiture'];
+$webgrp = $row['webgroup'];
+$gvtypename = $row['govtypename'];
+$desc = $row['description'];
+
+
+
+$get_governments = "select * from governments where GovId = '$gvid'";
+
+$run_governments = mysqli_query($con, $get_governments);
+
+$row_gov = mysqli_fetch_array($run_governments);
+
+
+$othertypeid = $row_gov['OtherIDtype'];
+
+$othertypecode = $row_gov['OtherID'];
+
+$rev = $row_gov['RevenueID'];
+
+$ktyabb = $row_gov['KtyAbb'];
+
+$fullspan = $row_gov['FullSpan'];
+
+$sort_as = $row_gov['NameSimple'];
+
+$parent = $row_gov['ParentID'];
+
+$version_details = "This is not the latest version";
+
+//$version_link = ", Click " . "<a href='#' class='text-white'>" . "here" . "</a> " . " to bring up latest version";
+
+$version_link =" ";
 
 ?>
 <!--no print div -->
 <div class="noprint">
-<!--header image-->
+    <!--header image-->
 
-<img src="images/screenblue.jpg" alt="" class="back-image">
+    <img src="images/screenblue.jpg" alt="" class="back-image">
 
-<!-- page title-->
+    <!-- page title-->
+    <div class="container">
+        <h2 class="page-title blue">
+            Screen Result
+        </h2>
+        <hr width="15%" class="page-title-line">
+
+    </div>
+</div>
+<!--table for ballot-->
 <div class="container">
-    <h2 class="page-title blue">
-        Screen Result
-    </h2>
-    <hr width="15%" class="page-title-line">
-
-  </div>
-  </div>
-    <!--table for ballot-->
-<div class="container">
-    <form method="POST" action="#" class="screen-border search-result address">
+    <form method="POST" class="screen-border search-result address">
 
         <!--row one-->
         <div class="row version">
             <div class="col-sm-1"></div>
             <div class="col-sm-10" style="font-weight: 600;">
-                <button style="font-weight: 600;" class="btn btn-md btn-danger"><?php echo $version_details; ?></button> 
+                <button style="font-weight: 600;" class="btn btn-md btn-danger"><?php echo $version_details; ?></button>
                 <span class="version-time">It is from <?php echo $timestamp . $version_link; ?> </span>
             </div>
         </div>
@@ -462,22 +546,23 @@ include('header.php');
                 <table class="table table-borderless text-white">
                     <tbody>
                         <tr>
-                            
+
                             <td>
                                 <br> <br>
-                                <a style="text-decoration: none;" class="btn btn-md btn-success" href="search.php" name="save">
+                                <a style="text-decoration: none;" class="btn btn-md btn-success" href="search.php"
+                                    name="save">
                                     <i class="fas fa-times"></i> Cancel</a>
                             </td>
 
                             <td>
                                 <br> <br>
-                                <button  onclick="window.print()" class="btn btn-md btn-warning">
+                                <button onclick="window.print()" class="btn btn-md btn-warning">
                                     <i class="fas fa-times"></i> Print</button>
                             </td>
 
                             <td>
-                                    
-                                        <?php
+
+                                <?php
 
                                                 $get_prevtime = "select timestamp from historytable WHERE govid ='$gvid' ORDER BY timestamp DESC";
                                                 
@@ -498,10 +583,11 @@ include('header.php');
                                                 ?>
 
 
-                            <span class="version-time" style="font-style: italic;"><?php echo $prev_timestamp; ?> </span><br><br>
-                                <a style="text-decoration: none;" class="btn btn-md btn-prev" href="#" name="save">
+                                <span class="version-time" style="font-style: italic;"><?php echo $prev_timestamp; ?>
+                                </span><br><br>
+                                <button class="btn btn-md btn-prev" name="previous">
 
-                                    <i class="fas fa-backward"></i> Get Previous Version</a>
+                                    <i class="fas fa-backward"></i> Get Previous Version</button>
                             </td>
 
                             <td>
@@ -570,5 +656,47 @@ include('header.php');
 
 
 <?php include('footer.php'); ?>
+<?php  } } ?> 
 
-<?php  } ?>
+<?php 
+
+if(isset($_POST['previous'])){
+
+    
+    $get_prev = "select * from historytable WHERE govid ='$gvid' ORDER BY timestamp DESC";
+                                                
+    $run_prev = mysqli_query($con , $get_prev);
+
+    if (mysqli_num_rows($run_prev) == 0){
+
+        echo '
+        <script>
+            swal({
+                    title: "No Previous Data!",
+                    icon: "error",
+                 });
+    </script>
+    ';
+
+    }
+    else{
+        $govid = "10278";
+
+        echo '
+        <script>
+            swal({
+                    title: "Previous Data Available!",
+                    icon: "success",
+                 });
+    </script>
+    ';
+
+    //echo "<script>window.open('search-result.php?govid=$govid', '_self')</script>";
+
+    }
+   
+
+
+}
+
+?>
