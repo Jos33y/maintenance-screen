9ada8134506645e43a15aa6b2fbb880c27f66c58
Jos33y <josephlagbalu@gmail.com>
2020-07-21 21:27:44 +0100

    update small

--- dbcon.php
+++ dbcon.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-//$con = mysqli_connect("localhost", "root", "", "citizenparticipation");
-$con = mysqli_connect("127.0.0.1:49841", "azure", "6#vWHD_$", "citizenparticipation");
+$con = mysqli_connect("localhost", "root", "", "citizenparticipation");
+//$con = mysqli_connect("127.0.0.1:49841", "azure", "6#vWHD_$", "citizenparticipation");
 
 define('TIMEZONE', 'America/Chicago');
 date_default_timezone_set(TIMEZONE);
